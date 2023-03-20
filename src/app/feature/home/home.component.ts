ef4192e6fafce77eebf99c92fa9df8cc16c1264b
daneilhpp <carvalhosilvad88@gmail.com>
2023-03-20 16:45:57 -0300

    [Commit04] HomePage complete, header links animation added

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -9,20 +9,42 @@ export class HomeComponent implements OnInit {
 
   greeting?: string;
   dateTime: Date = new Date();
-
+  faders = document.querySelectorAll('.textBox2');
+  
   constructor() { }
 
+  appearOptions = {
+    threshold: 1
+  };
+  appearOnScroll = new IntersectionObserver
+  (
+    function(entries, appearOnScroll)
+    {
+      entries.forEach(entry => {
+        if(!entry.isIntersecting)
+          return;
+        else{
+          entry.target.classList.add('appear');
+          appearOnScroll.unobserve(entry.target);
+        }
+      })
+    }, 
+    this.appearOptions
+  );
+
   ngOnInit(): void {
-    if(this.dateTime.getHours() > 6 && this.dateTime.getHours() <= 12){
+    if(this.dateTime.getHours() > 6 && this.dateTime.getHours() <= 11){
       this.greeting = 'Bom dia'
     }
-    else if(this.dateTime.getHours() > 12 && this.dateTime.getHours() <= 18){
+    else if(this.dateTime.getHours() > 11 && this.dateTime.getHours() <= 18){
       this.greeting = 'Boa tarde'
     } 
     else if(this.dateTime.getHours() > 18 && this.dateTime.getHours() <= 6){
       this.greeting = 'Boa noite'
     }
     console.log(this.dateTime.getHours())
-  }
 
+    this.faders.forEach(fader => this.appearOnScroll.observe(fader))
+  }
+  
 }
